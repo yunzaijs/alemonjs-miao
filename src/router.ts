ea5d898fec6c867096e63fa7d6e6494e15ab4489
ningmengchongshui <ningmengchongshui@gmail.com>
2026-03-29 01:38:20 +0800

    update: 仅处理消息

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,8 @@ import { defineRouter, lazy } from 'alemonjs';
 export default defineRouter([
   {
     // 局部中间件
+    // 仅限消息和交互事件处理
+    selects: ['private.message.create', 'message.create', 'interaction.create', 'private.interaction.create'],
     handler: lazy(() => import('@src/response/mw')),
     children: [
       // ─── 帮助 ──────────────────────────────────────
